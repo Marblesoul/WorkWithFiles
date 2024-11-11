1905c878a8de25a8dc68db6deec572986bddfc76
Dmitry <gworking7@gmail.com>
2024-11-11 17:12:18 +0300

    Task #1 Getting cook_book from input_data.txt

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import os
 from pprint import pprint
 
-cook_book = {}
+file_path = os.path.join(os.getcwd(), 'input_data.txt')
 
+cook_book = {}
 temp_dish = []
 
-with open('input_data.txt', 'r', encoding='utf-8') as file:
+with open(file_path, 'r', encoding='utf-8') as file:
     lines = file.read().splitlines()
+    for line in lines:
+        if not line:
+            cook_book[temp_dish[0]] = temp_dish[2:]
+            temp_dish.clear()
+        elif '|' in line:
+            ingredient_name, quantity, measure = line.split(' | ')
+            temp_dish.append(
+                dict(ingredient_name=ingredient_name.strip(),
+                     quantity=int(quantity),
+                     measure=measure.strip()))
+        else:
+            temp_dish.append(line)
+    else:
+        cook_book[temp_dish[0]] = temp_dish[2:]
+
+pprint(cook_book)
